211ff6fd965eb5bcba00bcdbd1778d6ec1dc24fc
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-05-06 19:08:25 -0300

    style: root variáveis e estilização default

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
-
 export default {
   name: 'App',
 };
 </script>
 
 <style>
+@font-face {
+  font-family: 'Press Start 2P';
+  src: url('@/assets/fonts/PressStart2P-Regular.ttf');
+}
 
+:root {
+  --primary: #B946FF;
+  --accent: #26FF8A;
+  --dark: #1C1C1E;
+  --white: #FFFFFF;
+  --pressStart: 'Press Start 2P', sans-serif;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+
+  width: 100%;
+  height: 100vh;
+}
+
+.container-screen {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+}
 </style>
