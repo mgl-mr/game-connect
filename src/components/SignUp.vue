e0fd814f846f78fa0c5915da651a086c523de83c
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-05-15 16:32:53 -0300

    feat: add ProgressBar

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
+  <div class="container">
+    <ProgressBar />
     <router-view />
   </div>
 </template>
 
 <script>
+import ProgressBar from './ProgressBar.vue';
+
 export default {
+  components: { ProgressBar },
   name: 'SignUp',
 };
 </script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+</style>
