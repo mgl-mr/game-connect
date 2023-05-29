96341d179d123bd0f940557ea66a888165e127dd
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-05-29 18:01:12 -0300

    feat: fetchJogos + signup

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,27 @@
-import { auth, database } from '@/firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { ref, get } from 'firebase/database';
+/* eslint-disable no-param-reassign */
+import {
+  auth,
+  database,
+  storage,
+} from '@/firebase';
+
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from 'firebase/auth';
+
+import {
+  ref,
+  get,
+  set,
+} from 'firebase/database';
+
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from 'firebase/storage';
 
 export default {
   async login({ commit }, userData) {
@@ -12,7 +33,7 @@ export default {
       );
 
       if (credentials.user.emailVerified) {
-        const userRef = ref(database, `jogadores/${credentials.user.uid}`);
+        const userRef = ref(database, `gamers/${credentials.user.uid}`);
         const snapshot = await get(userRef);
         if (snapshot.exists()) {
           const user = snapshot.val();
@@ -25,7 +46,83 @@ export default {
       return 'Você deve verificar o seu email.';
     } catch (error) {
       console.log(error);
-      return 'Emal ou senha icorreto(s).';
+      return 'Email ou senha icorreto(s).';
+    }
+  },
+
+  async signUp({ commit }, userData) {
+    try {
+      let downloadURL = '';
+      const userAuth = await createUserWithEmailAndPassword(
+        auth,
+        userData.email,
+        userData.password,
+      );
+
+      if (userData.image !== '') {
+        const imageName = userData.name.replace(/\s/g, '').substring(0, 3) + userAuth.user.uid.substring(0, 10);
+        const imageRef = storageRef(storage, `/${imageName}`);
+
+        await uploadBytes(imageRef, userData.image);
+        downloadURL = await getDownloadURL(imageRef);
+      }
+
+      const databaseRef = ref(database, `gamers/${userAuth.user.uid}`);
+      await set(databaseRef, {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        birthdate: userData.birthdate,
+        bio: userData.bio,
+        start: userData.start,
+        end: userData.end,
+        imageURL: downloadURL,
+        games: userData.games,
+      });
+
+      sendEmailVerification(userAuth.user);
+
+      commit('setUserSignup', {
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        birthdate: '',
+        bio: '',
+        start: '',
+        end: '',
+        image: '',
+        imageName: '',
+        urlImage: '',
+        games: [],
+      });
+
+      return true;
+    } catch (error) {
+      if (error.code && error.code.startsWith('storage/')) {
+        return 'Erro ao cadastrar usuário. Problema ao enviar imagem.';
+      }
+
+      if (error.code && error.code === 'auth/email-already-in-use') {
+        return 'Email já está em uso.';
+      }
+
+      return 'Erro ao registrar conta.';
+    }
+  },
+
+  async fetchJogos({ commit }) {
+    try {
+      const gamesRef = ref(database, 'jogos');
+      const snapshot = await get(gamesRef);
+      const gamesArray = Object.entries(snapshot.val()).map(([id, game]) => {
+        game.id = id;
+        return game;
+      });
+
+      commit('setGames', gamesArray);
+    } catch (error) {
+      console.error(`actions, fetchJogos: ${error}`);
     }
   },
 };
