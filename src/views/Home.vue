8eb930e28934738290771f2e18a46884b63b3b9e
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-05-29 21:35:00 -0300

    feat: estrutura inicial Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-screen">
+    <p>Novas features em breve!!!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+.container-screen{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(180deg, var(--dark), var(--primary));
+}
+
+p {
+  color: var(--white);
+  font-family: var(--pressStart), Arial, Helvetica;
+}
+</style>
