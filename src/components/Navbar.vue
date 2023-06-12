3219064d10c4877035e1af6d42031e67e27fa76c
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-06-12 20:24:51 -0300

    feat: Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-nav">
+    <router-link
+      to="/sign-in"
+      :class="{
+        'auth-link-selected':checkRoute('/my-profile'),
+        'auth-link':!checkRoute('/my-profile')}"
+    >
+      Meu perfil
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+
+  methods: {
+    checkRoute(route) {
+      return this.$route.path === route;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container-nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 15vh;
+
+  border-bottom: 5px solid var(--primary);
+}
+
+.auth-link {
+  text-decoration: none;
+  font-family: var(--pressStart);
+  font-size:  calc(1vw + 0.5vh);
+  color: var(--accent);
+  transition: transform 0.2s ease-in-out;
+}
+
+.auth-link:hover {
+  transform: scale(1.1);
+  background-image: linear-gradient(45deg, var(--primary), var(--accent));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+
+.auth-link-selected {
+  text-decoration: none;
+  height: 6vh;
+  align-self: flex-end;
+  padding: calc(1vw + 0.5vh);
+
+  font-family: var(--pressStart);
+  font-size: calc(1vw + 0.5vh + 4px);
+  color: var(--accent);
+  text-shadow: 0px 4px 4px var(--primary);
+  background: rgba(185, 70, 255, 0.1);
+  box-shadow: inset 0px 4px 4px #B946FF;
+  border-radius: 15px 15px 0px 0px;
+}
+</style>
