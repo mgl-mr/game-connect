c9ae410cb6ac4e7dfe331889899977f12b53466e
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-06-12 20:25:50 -0300

    feat: chamado dos componentes de estruturação

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container-screen">
     <Sidebar />
+    <div class="application">
+      <Navbar />
+      <router-view class="component" />
+    </div>
   </div>
 </template>
 
 <script>
 import Sidebar from '@/components/Sidebar.vue';
+import Navbar from '@/components/Navbar.vue';
 
 export default {
   name: 'Application',
-  components: { Sidebar },
+  components: { Sidebar, Navbar },
 
   async mounted() {
     if (this.$store.state.user.friendsId.length !== 0) {
@@ -27,8 +32,14 @@ export default {
   background: linear-gradient(180deg, var(--dark), var(--primary));
 }
 
-p {
-  color: var(--white);
-  font-family: var(--pressStart), Arial, Helvetica;
+.application {
+  width: 75%;
+  height: 100vh;
+  background: linear-gradient(180deg, var(--dark) 0%, var(--primary) 100%);
+}
+
+.component {
+  width: 100%;
+  height: 80vh;
 }
 </style>
